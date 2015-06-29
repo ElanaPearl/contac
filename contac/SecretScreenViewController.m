0cecb880571077ebbfce1346a8d6ae1d2acd7abe
katelynnkyker <katelynn.kyker@yale.edu>
2015-06-29 13:46:39 -0700

    add gitignore

---
+++ contac/SecretScreenViewController.m
@@ -0,0 +1,18 @@
+//
+//  SecretScreenViewController.m
+//  contac
+//
+//  Created by Katelynn Kyker on 6/29/15.
+//  Copyright (c) 2015 Contact+ Inc. All rights reserved.
+//
+
+#import "SecretScreenViewController.h"
+
+@interface SecretScreenViewController ()
+
+@end
+
+@implementation SecretScreenViewController
+
+
+@end
