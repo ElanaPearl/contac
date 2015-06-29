3df30aa374219228e5945c69f794249fe78c290d
elanasimon95@gmail.com <elanasimon@elanasimon-mba.dhcp.thefacebook.com>
2015-06-29 15:12:13 -0700

    added basic voice recording and playing functionality

---
+++ AudioViewController.m
@@ -0,0 +1,111 @@
+//
+//  AudioViewController.m
+//  contac
+//
+//  Created by Elana Simon on 6/29/15.
+//  Copyright (c) 2015 Contact+ Inc. All rights reserved.
+//
+
+#import "AudioViewController.h"
+
+@interface AudioViewController () {
+    AVAudioRecorder *recorder;
+    AVAudioPlayer *player;
+}
+
+@end
+
+@implementation AudioViewController
+@synthesize stopButton, playButton, recordButton;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Disable Stop/Play button when application launches
+    [stopButton setEnabled:NO];
+    [playButton setEnabled:NO];
+    
+    // Set the audio file
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"MyAudioMemo.m4a",
+                               nil];
+    NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    
+    // Setup audio session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    
+    // Define the recorder setting
+    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc] init];
+    
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    // Initiate and prepare the recorder
+    recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:NULL];
+    recorder.delegate = self;
+    recorder.meteringEnabled = YES;
+    [recorder prepareToRecord];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)recordTapped:(id)sender {
+    // Stop the audio player before recording
+    if (player.playing) {
+        [player stop];
+    }
+    
+    if (!recorder.recording) {
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        [session setActive:YES error:nil];
+        
+        // Start recording
+        [recorder record];
+        
+    }
+    
+    [stopButton setEnabled:YES];
+    [playButton setEnabled:NO];
+}
+
+- (IBAction)stopTapped:(id)sender {
+    [recorder stop];
+    
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:NO error:nil];
+}
+
+- (IBAction)playTapped:(id)sender {
+    if (!recorder.recording){
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+        [player setDelegate:self];
+        [player play];
+    }
+}
+
+- (void) audioRecorderDidFinishRecording:(AVAudioRecorder *)avrecorder successfully:(BOOL)flag{
+    [recordButton setTitle:@"Record" forState:UIControlStateNormal];
+    
+    [stopButton setEnabled:NO];
+    [playButton setEnabled:YES];
+}
+
+//- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag{
+//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Done"
+//                                                    message: @"Finish playing the recording!"
+//                                                   delegate: nil
+//                                          cancelButtonTitle:@"OK"
+//                                          otherButtonTitles:nil];
+//    [alert show];
+//}
+
+
+@end
