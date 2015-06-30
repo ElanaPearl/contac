53fffbc0d1ffbc081af22313f25151e1abff79e8
Elana Simon <elanasimon95@gmail.com>
2015-06-29 17:09:13 -0700

    added voice stuff again to add_contac

---
+++ Contac+/Contac+/AudioViewController.h
@@ -0,0 +1,24 @@
+//
+//  AudioViewController.h
+//  contac
+//
+//  Created by Elana Simon on 6/29/15.
+//  Copyright (c) 2015 Contact+ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface AudioViewController : UIViewController <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIButton *recordButton;
+@property (weak, nonatomic) IBOutlet UIButton *stopButton;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+
+- (IBAction)recordTapped:(id)sender;
+- (IBAction)stopTapped:(id)sender;
+- (IBAction)playTapped:(id)sender;
+
+@end
+
